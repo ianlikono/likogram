7aed5840fad29734f3f8b591474f8a3139caed45
root <root@localhost.localdomain>
2017-10-19 20:33:25 +0300

    Halfway Cant wait to finish

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :owned_post, only: [:edit, :update, :destroy]
+	def index
+		@posts=Post.all
+		
+	end
+	def new
+		@post = current_user.posts.build
+		
+	end
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			flash[:success] = "Your post has been created!"
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your new post couldn't be created!
+			check the form."
+			render 'new'
+					
+				end		
+	end
+	def show
+				
+	end
+	def edit
+			
+	end
+	def update
+		if @post.update(post_params)
+           flash[:success] = "Post updated."
+           redirect_to posts_path
+       else
+           flash.now[:alert] = "Update failed."
+           render :edit
+        end
+    end
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+		
+	end
+	private
+	def post_params
+		params.require(:post).permit(:image, :caption)		
+	end
+	def set_post
+		@post=Post.find(params[:id])		
+	end
+	def owned_post
+	unless current_user == @post.user
+	flash[:alert] = "That post doesn't belong to you!"
+	redirect_to root_path
+	end
+end
+end		
+
+
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+	belongs_to :user
+	has_many :comments, dependent: :destroy
+	validates :user_id, presence: true
+	validates :image, presence: true
+	validates :caption, length: {in: 3..300}
+	has_attached_file :image, styles: { medium: "640x" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
